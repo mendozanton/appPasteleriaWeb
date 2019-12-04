c2740d4c948eb354a6cf7b8616309e511dc63701
abraham <abrmendoza32@gmail.com>
2019-12-04 07:08:39 -0500

    primario

---
+++ app/app.js
@@ -0,0 +1,16 @@
+var app = angular
+    .module('app',
+    ['ui.router'])
+    .config(config);
+    //.run(run);
+
+function config($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/modules/principal/login.html',
+            controller: 'principalController',
+            controllerAs: 'vm'
+        })
+}
